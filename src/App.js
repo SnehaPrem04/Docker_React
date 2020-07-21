02f180e2349ea87cf7e6a5b795ddfe49a010d386
SnehaPrem04 <snehaprem04@yahoo.com>
2020-07-21 12:23:36 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Snaksdfgh<code>src/App.js</code> and save to reload.
+        Snakey math<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
